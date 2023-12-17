d91e35f020a0dec952bdddcd134dcf0e671dbb69
J.H <jiyue_he@163.com>
2023-12-17 01:12:05 -0500

    javascript header

---
+++ header.js
@@ -0,0 +1,14 @@
+const template = document.createElement('template');
+
+template.innerHTML = `
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Jiyue He</title>
+<link rel="stylesheet" href="style.css" />
+<link rel="stylesheet" href="mediaqueries.css" />
+</head>
+`;
+
+document.body.appendChild(template.content);
