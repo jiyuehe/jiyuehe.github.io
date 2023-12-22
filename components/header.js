ff7d61163457a473e81aa9433ce3d1b600686c35
J.H <jiyue_he@163.com>
2023-12-22 10:24:11 -0500

    .

--- components/header.js
+++ components/header.js
@@ -6,11 +6,11 @@ class Header extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `
       <head>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta charset="UTF-8"/>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <title>Jiyue He</title>
-      <link rel="stylesheet" href="style.css" />
+      <link rel="stylesheet" href="components/style.css"/>
       </head>
       `;
     }
