b81dbdf653177dc959bf166fd3ca0f615d9b31b7
J.H. <jiyue_he@163.com>
2024-11-06 11:26:47 -0500

    style update

--- components/header.js
+++ components/header.js
@@ -9,7 +9,7 @@ class Header extends HTMLElement {
       <meta charset="UTF-8"/>
       <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <title>Jay He</title>
+      <title>J. He</title>
       </head>
       `;
     }
