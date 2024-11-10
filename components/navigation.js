df006aca2b9393d2c97d0de4d484263b9795341d
J.H. <jiyue_he@163.com>
2024-11-10 10:55:59 -0500

    structural

--- components/navigation.js
+++ components/navigation.js
@@ -20,9 +20,13 @@ class Navigation extends HTMLElement {
                 <i class="fa fa-caret-down"></i>
               </button>
               <div class="dropdown-content">
-                <a href="clinical_data_processing_and_arrhythmia_detection.html">Clinical Data Processing and Arrhythmia Source Detection</a>
-                <a href="patient_specific_heart_modeling.html">Model Patient-specific Heart for Arrhythmia Ablation</a>
-                <a href="fiber_effect.html">Model Myocardial Fiber Effects on Arrhythmia Patterns</a>
+                <a href="clinical_processing_arrhythmia_detection.html">Algorithm Development: Clinical Data Processing and Arrhythmia Source Detection</a>
+                <a href="patient_specific_heart_modeling.html">Modeling: Patient-specific Digital Heart for Arrhythmia Ablation</a>
+                <a href="fiber_effect.html">Modeling: Myocardial Fiber Effects on Arrhythmia Patterns</a>
+                <a href="egm_classification_anatomic_segment.html">Machine Learning: Electrogram Classification and Anatomical Segmentation</a>
+                <a href="voltage_map_comparison.html">Algorithm Development: Voltage Map Comparison</a>
+                <a href="signature_verification.html">Machine Learning: Handwritten Digital Signature Verification</a>
+                <a href="micro_robot.html">Modeling: Electromagnetic Micro Robot</a>
               </div>
             </div> 
 
