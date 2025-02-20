b182ec6401ac1307cf8dc19e1f3acf72aa253ef0
J.H. <jiyue_he@163.com>
2025-02-19 21:09:52 -0500

    add house renovation

--- components/navigation.js
+++ components/navigation.js
@@ -23,13 +23,22 @@ class Navigation extends HTMLElement {
                 <a href="clinical_processing_arrhythmia_detection.html">Clinical Data Processing and Arrhythmia Source Detection</a>
                 <a href="patient_specific_heart_modeling.html">Patient-specific Digital Heart for Arrhythmia Ablation</a>
                 <a href="fiber_effect.html">Myocardial Fiber Effects on Arrhythmia Patterns</a>
-                <!--<a href="template_page.html">title</a>-->
               </div>
             </div> 
 
             <a href="publications.html">Publications</a>
 
             <a href="awards.html">Awards</a>
+
+            <div class="dropdown">
+              <button class="dropbtn">More
+                <i class="fa fa-caret-down"></i>
+              </button>
+              <div class="dropdown-content">
+                <a href="house_renovation.html">Renovated a House</a>
+                <a href="offroading.html">Off-roading</a>
+              </div>
+            </div> 
           </div>
         </td>
       </tr>
