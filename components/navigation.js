50cb3cf8fde629407fa992d0a0cd51cfb5394de9
J.H. <jiyue_he@163.com>
2025-03-05 14:59:10 -0500

    Update navigation.js

--- components/navigation.js
+++ components/navigation.js
@@ -16,11 +16,13 @@ class Navigation extends HTMLElement {
                 <i class="fa fa-caret-down"></i>
               </button>
               <div class="dropdown-content">
-                <a href="clinical_processing_arrhythmia_detection.html">Clinical Data Processing and Arrhythmia Source Detection</a>
+                <a href="atrial_fibrillation_challenges.html">Atrial Fibrillation Ablation Challenges</a>
 
                 <a href="patient_specific_heart_modeling.html">Patient-specific Digital Heart for Arrhythmia Ablation</a>
 
                 <a href="fiber_effect.html">Myocardial Fiber Effects on Arrhythmia Patterns</a>
+
+                <a href="clinical_processing_arrhythmia_detection.html">Clinical Data Processing and Arrhythmia Source Detection</a>
 
                 <a href="statistical_activation_mapping.html">Atrial Fibrillation Statistical Activation Mapping</a>
 
